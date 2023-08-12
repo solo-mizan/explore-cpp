d7b28282e0a5055878947e2b1742bf81e990377d
solo-mizan <mizanbd.eee@gmail.com>
2023-08-12 22:17:21 +0600

    switch case statement

--- tut-9-if-else-switch-case.cpp
+++ tut-9-if-else-switch-case.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main () {
 
-    // ********* if, else if and else statement *********
+    // ********* if, else-if and else statement *********
     int age;
     cout << "Enter your age below:" << endl;
     cin >> age;
@@ -16,6 +16,21 @@ int main () {
     }
     else{
         cout << "Welcome to the party." << endl;
+    }
+
+    // ******** switch case statement ********
+    switch (age)
+    {
+    case 18:
+        cout << "Your age is 18" << endl;
+        break;
+        case 20:
+        cout << "Your age is greater than 18" << endl;
+        break;
+
+    default:
+        cout << "No special cases" << endl;
+        break;
     }
 
 return 0;
