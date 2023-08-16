1819c2e9bb5088d1b800c0c266d710f2ef381b17
solo-mizan <mizanbd.eee@gmail.com>
2023-08-16 23:34:36 +0600

    static variable

--- tut-17-inline-functions-default-arguments-constant-arguments.cpp
+++ tut-17-inline-functions-default-arguments-constant-arguments.cpp
@@ -8,13 +8,26 @@ advantages of declaring a inline function:
 
 // inline function 
 inline int product(int x, int y){
-    return x * y;
+    static int c = 0; // This line will execute only once.
+    c = c + 1;        // This function will remember last "c" value.
+    return (x * y) + c;
 }
 
 int main () {
     int a, b;
     cout << "Enter the value of a and b" << endl;
     cin >> a >> b;
-    cout << "The product of a and b is " << product(a, b);
+    cout << "The product of a and b is " << product(a, b)<< endl;
+    cout << "The product of a and b is " << product(a, b)<< endl;
+    cout << "The product of a and b is " << product(a, b)<< endl;
+    cout << "The product of a and b is " << product(a, b)<< endl;
+    cout << "The product of a and b is " << product(a, b)<< endl;
+    cout << "The product of a and b is " << product(a, b)<< endl;
+    cout << "The product of a and b is " << product(a, b)<< endl;
+    cout << "The product of a and b is " << product(a, b)<< endl;
+    cout << "The product of a and b is " << product(a, b)<< endl;
+    cout << "The product of a and b is " << product(a, b)<< endl;
+    cout << "The product of a and b is " << product(a, b)<< endl;
+
     return 0;
 }
