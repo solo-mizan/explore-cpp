b760bba10ec1cb564fbbfcba883f7852fb3b7094
solo-mizan <mizanbd.eee@gmail.com>
2023-08-19 23:56:44 +0600

    OOP clas

---
+++ tut-21-classes-public-private-access-modifier.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+// create a student class
+class Student{
+private:
+    string gfName;
+    float cgpa;
+
+public:
+    string name;
+    int id;
+    void getData(){
+        cout << "Enter student name: \n";
+        cin >> name;
+        cout << "Enter student ID: " << endl;
+        cin >> id;
+        cout << "Enter your friend name: " << endl;
+        cin >> gfName;
+        cout << "Enter your CGPA: " << endl;
+        cin >> cgpa;
+    }
+
+    void printData(){
+        cout << "Name: " << name << endl;
+        cout << "ID: " << id << endl;
+        cout << "Friend name: " << gfName << endl;
+        cout << "CGPA: " << cgpa << endl;
+    };
+};
+
+int main () {
+
+    // create a student class object
+    Student Mizan;
+    Mizan.getData();
+    Mizan.printData();
+
+    return 0;
+}
