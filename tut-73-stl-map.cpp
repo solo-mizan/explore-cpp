45adc82cf6cc38fdc569ba8fcacf825cb8f9d4a3
solo-mizan <mizanbd.eee@gmail.com>
2024-02-19 02:22:11 +0600

    stl map methods

--- tut-73-stl-map.cpp
+++ tut-73-stl-map.cpp
@@ -4,17 +4,31 @@
 using namespace std;
 
 int main () {
+
+    // declare and initial a map -->
     map<string, int> phoneMap;
     phoneMap["Mizan"] = 54586;
     phoneMap["Badhon"] = 89523;
     phoneMap["Niloy"] = 42658;
     phoneMap["Santo"] = 369855;
 
+    // insert some key, value pair in map -->
+    phoneMap.insert({{"Shakib", 2312321}, {"Mujahid", 6586532}});
+
+    // make iterator -->
     map<string, int>::iterator iter;
 
+    // iterate through map -->
     for (iter = phoneMap.begin(); iter != phoneMap.end(); iter++){
         cout << (*iter).first << " " << (*iter).second << endl;
     }
 
-        return 0;
+    // map size -->
+    cout << "phoneMap size is " << phoneMap.size() << endl;
+    // is the map empty? -->
+    cout << "Is the map empty? --> " << phoneMap.empty() << endl;
+    // max size of a map -->
+    cout << "Max size of a map is " << phoneMap.max_size() << endl;
+
+    return 0;
 }
